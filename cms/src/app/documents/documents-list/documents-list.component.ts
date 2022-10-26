0e27ebf59cc6a7e755adfdc74885004cb801ec74
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2022-10-26 14:09:17 -0400

    documents component is working with observables

--- cms/src/app/documents/documents-list/documents-list.component.ts
+++ cms/src/app/documents/documents-list/documents-list.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { Document } from '../documents.model';
 import { DocumentsService } from '../documents.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-documents-list',
   templateUrl: './documents-list.component.html',
   styleUrls: ['./documents-list.component.css'],
 })
-export class DocumentsListComponent implements OnInit {
+export class DocumentsListComponent implements OnInit, OnDestroy {
+  private subscription!: Subscription;
   documents: Document[] = [];
 
   constructor(private documentsService: DocumentsService) {}
 
   ngOnInit(): void {
     this.documents = this.documentsService.getDocuments();
-    this.documentsService.documentSelectedEvent.subscribe(
+    this.subscription = this.documentsService.documentChangedEvent.subscribe(
       (documents: Document[]) => {
         this.documents = documents;
       }
     );
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
--- cms/src/app/documents/documents.service.ts
+++ cms/src/app/documents/documents.service.ts
@@ -1,5 +1,6 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Document } from './documents.model';
+import { Subject } from 'rxjs';
 import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
 
 @Injectable({
@@ -7,11 +8,13 @@ import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
 })
 export class DocumentsService {
   documentSelectedEvent = new EventEmitter<Document>();
-  documentChangedEvent = new EventEmitter<Document>();
+  documentChangedEvent = new Subject<Document[]>();
   documents: any; //kept erroring out if I didn't make this any
+  maxDocumentId: number;
 
   constructor() {
     this.documents = MOCKDOCUMENTS;
+    this.maxDocumentId = this.getMaxId();
   }
 
   getDocuments(): Document[] {
@@ -36,6 +39,45 @@ export class DocumentsService {
       return;
     }
     this.documents.splice(pos, 1);
-    this.documentChangedEvent.emit(this.documents.slice());
+    this.documentChangedEvent.next(this.documents.slice());
+  }
+
+  
+  getMaxId(): number {
+    let maxId = 0;
+    for (const document of this.documents) {
+      const currentId = +document.id;
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+    return maxId;
+  }
+
+  
+  addDocument(newDocument: Document) {
+    if (newDocument === undefined || newDocument === null) {
+      return;
+    }
+    this.maxDocumentId++;
+    newDocument.id = this.maxDocumentId.toString();
+    this.documents.push(newDocument);
+    const documentListClone = this.documents.slice();
+
+    this.documentChangedEvent.next(documentListClone);
+  }
+
+  updateDocument(originalDocument: Document, newDocument: Document) {
+    if (originalDocument === undefined || originalDocument === null || newDocument === undefined || newDocument === null) {
+      return;
+    }
+    const pos = this.documents.indexOf(originalDocument);
+    if (pos < 0) {
+      return;
+    }
+    newDocument.id = originalDocument.id;
+    this.documents[pos] = newDocument;
+    const documentListClone = this.documents.slice();
+    this.documentChangedEvent.next(documentListClone);
   }
 }
